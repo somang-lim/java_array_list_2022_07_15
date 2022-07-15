72236c40c5615284ee26f073c03c6c3b1db07938
somang-lim <somangjuha@gmail.com>
2022-07-15 10:42:38 +0900

    ArrayList 클래스 생성

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,4 +1,12 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
+
+    @Test
+    void ArrayList_클래스가_존재한다() {
+        ArrayList al = new ArrayList();
+    }
+
 }
