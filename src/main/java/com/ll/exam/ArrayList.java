a385d0044f0ee25059c9327340eec8bde214090a
somang-lim <somangjuha@gmail.com>
2022-07-15 10:46:12 +0900

    size 메서드 구현

--- src/main/java/com/ll/exam/ArrayList.java
+++ src/main/java/com/ll/exam/ArrayList.java
@@ -1,4 +1,22 @@
 package com.ll.exam;
 
 public class ArrayList {
+
+    private int size;
+
+    public ArrayList() {
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(int datum) {
+        size++;
+    }
+
+    public int get(int index) {
+        return 100;
+    }
 }
